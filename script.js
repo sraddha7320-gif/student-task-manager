4cf2ac12bc2ab6fc63fed0bb6d4e68b501054115
Shraddha Suman Pati <sraddha7320@gmail.com>
2026-01-02 23:34:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value;
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerText = taskText;
+
+    li.onclick = function () {
+        this.remove();
+    };
+
+    document.getElementById("taskList").appendChild(li);
+    taskInput.value = "";
+}
 
